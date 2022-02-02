69984f25b39a5fc37d44f454b12e86076817d0e4
pb3899 <92201539+pb3899@users.noreply.github.com>
2022-02-02 19:57:56 +0530

    Create ageleft.py

---
+++ ageleft.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+AGE=int(age)
+years = 90 - AGE
+days = years * 365
+months = years * 12
+print(f"you have {years} ,years or {months}, months or {days}, days")
