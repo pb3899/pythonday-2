5ececa262eb6005d27e852cdb46771862f661b7c
pb3899 <92201539+pb3899@users.noreply.github.com>
2022-02-02 19:57:25 +0530

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+new_height=float(height)
+new_weight=int(weight)
+#d=new_height*new_height
+bmi=new_weight/new_height**2
+BMI=int(bmi)
+print(BMI)
+#Write your code below this line 👇
