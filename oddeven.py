f56945aa172a4a85971b09830e96aea10c10b9b8
pb3899 <92201539+pb3899@users.noreply.github.com>
2022-02-02 20:00:31 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+mod=number%2
+if mod == 0:
+ print("number is even")
+else:
+ print("number is odd")
