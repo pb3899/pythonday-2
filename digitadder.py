90f54a02213d3f11676d32e6d6a7a2ff917e6496
pb3899 <92201539+pb3899@users.noreply.github.com>
2022-02-02 19:56:42 +0530

    Create digitadder.py

---
+++ digitadder.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+firtdigit=two_digit_number[0]
+seconddigit=two_digit_number[1]
+first=int(firtdigit)
+second=int(seconddigit)
+result=first + second 
+print(result)
